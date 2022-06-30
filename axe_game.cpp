b05fcd9fd86356eb1f8b6fc1ac468d7b69753f7f
dawkey95 <dawidkeyser95@gmail.com>
2022-07-01 09:06:48 +1200

    Axe Game created learning about variables

---
+++ axe_game.cpp
@@ -0,0 +1,22 @@
+#include "raylib.h"
+
+int main()
+{
+
+    //Variables
+    int width;
+    int height;
+    float rootBeer;
+    double cheeseBurger;
+    bool ShouldHaveLunch;
+
+    //Variables insantiated
+    width = 350;
+    height = 200;
+    rootBeer = 1.99;
+    cheeseBurger = 5.99;
+    ShouldHaveLunch = true;
+
+    //Main Code
+    InitWindow(width, height, "Axe Game");
+}
