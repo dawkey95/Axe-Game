4384fd79c69ef2dc66350c341712896c7ec815dd
dawkey95 <dawidkeyser95@gmail.com>
2022-07-01 09:45:20 +1200

    Variable Update

--- axe_game.cpp
+++ axe_game.cpp
@@ -6,16 +6,10 @@ int main()
     //Variables
     int width;
     int height;
-    float rootBeer;
-    double cheeseBurger;
-    bool ShouldHaveLunch;
 
     //Variables Initialized
     width = 350;
     height = 200;
-    rootBeer = 1.99;
-    cheeseBurger = 5.99;
-    ShouldHaveLunch = true;
 
     //Main Code
     InitWindow(width, height, "Axe Game");
