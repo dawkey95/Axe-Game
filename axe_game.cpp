4ef3d9b8f2f43d90a2c6b6596f94ca7afc4cbcc2
dawkey95 <dawidkeyser95@gmail.com>
2022-07-15 10:53:13 +1200

    Added second obstacle - Need fix scoring and colouring

--- axe_game.cpp
+++ axe_game.cpp
@@ -16,19 +16,36 @@
     int obstacleRadius = 25;
     float directionV = 5;
     float directionH = 5;
+    
+    //  Obstacle Circle Second
+    int obstacleCenterXSecond = 200;
+    int obstacleCenterYSecond = 50;
+    int obstacleRadiusSecond = 25;
+    float directionVSecond = 7;
+    float directionHSecond = 3;
 
     //  Collisions
     float tolerance = playerRadius + obstacleRadius;
+    float toleranceSecond = playerRadius + obstacleRadiusSecond;
+
     float goal = (5 * playerRadius) + obstacleRadius;
+    float goalSecond = (5 * playerRadius) + obstacleRadiusSecond;
+
     bool collisionWithObstacle{};
+    bool collisionWithObstacleSecond{};
+
     bool collisionWithScoringZone{};
+    bool collisionWithScoringZoneSecond{};
+
     float distance{};
+    float distanceSecond{};
 
     //  Color Definitions
     Color CYAN = {0, 255, 255, 255};
     Color ScoreColor = CYAN;
     Color PlayerColor = DARKGRAY;
     Color ObstacleColor = MAROON;
+    Color ObstacleColorSecond = RED;
     Color BackgroundColor = BLACK;
     Color Flicker[]{DARKGRAY, GRAY, LIGHTGRAY, WHITE};
     int FlickerSize = 4;
@@ -83,6 +100,10 @@ void UpdateDistanceCheck()
     distance = sqrt(pow(playerCenterX - obstacleCenterX, 2) + pow(playerCenterY - obstacleCenterY, 2));
     collisionWithObstacle = distance < tolerance;
     collisionWithScoringZone = distance < goal;
+
+    distanceSecond = sqrt(pow(playerCenterX - obstacleCenterXSecond, 2) + pow(playerCenterY - obstacleCenterYSecond, 2));
+    collisionWithObstacleSecond = distanceSecond < toleranceSecond;
+    collisionWithScoringZoneSecond = distanceSecond < goalSecond;
 }
 
 void ResetGameVariables()
@@ -91,9 +112,12 @@ void ResetGameVariables()
     playerCenterY = 200;
     obstacleCenterX = 400;
     obstacleCenterY = 25; 
+    obstacleCenterXSecond = 200;
+    obstacleCenterYSecond = 50; 
     Timer = Delay;
     Score = 0;
     collisionWithObstacle = false;
+    collisionWithObstacleSecond = false;
 }
 
 void CalculateAndIndicateScoring()
@@ -101,17 +125,19 @@ void CalculateAndIndicateScoring()
     if (collisionWithScoringZone)
     {
         // increase score based on the distance from the obstacle but never less than 1 
-        Score += ((goal-distance)/playerRadius)+1;
+        Score += ((goal - distance) / playerRadius) + 1;
         // recolor the obstacle
         DrawCircle(obstacleCenterX, obstacleCenterY, obstacleRadius, ScoreColor);   
+        DrawCircle(obstacleCenterXSecond, obstacleCenterYSecond, obstacleRadiusSecond, ScoreColor);
     }
 }
 
 void DrawScorePlayerAndObstacle()
 {
-    DrawText(TextFormat("Score: %i", Score), windowWidth*.85, windowHeight*.9, 20, ScoreColor);
+    DrawText(TextFormat("Score: %i", Score), windowWidth * .85, windowHeight * .9, 20, ScoreColor);
     DrawCircle(playerCenterX, playerCenterY, playerRadius, PlayerColor);
     DrawCircle(obstacleCenterX, obstacleCenterY, obstacleRadius, ObstacleColor);
+    DrawCircle(obstacleCenterXSecond, obstacleCenterYSecond, obstacleRadiusSecond, ObstacleColorSecond);
 }
 
 void BouncyBallMovement()
@@ -126,7 +152,18 @@ void BouncyBallMovement()
     {
         directionH = -directionH;
     }    
-}
+
+    obstacleCenterYSecond += directionVSecond;
+    if (obstacleCenterYSecond > (windowHeight - obstacleRadiusSecond) || (obstacleCenterYSecond < obstacleRadiusSecond))
+    {
+        directionVSecond = -directionVSecond;
+    }
+    obstacleCenterXSecond += directionHSecond;
+    if (obstacleCenterXSecond > (windowWidth - obstacleRadiusSecond) || (obstacleCenterXSecond < obstacleRadiusSecond))
+    {
+        directionHSecond = -directionHSecond;
+    }   
+}     
 
 void ProcessGameControls()
 {
@@ -170,7 +207,7 @@ int main()
         else
         {
             // End Screen   
-            if(collisionWithObstacle)
+            if(collisionWithObstacle || collisionWithObstacleSecond)
             {
                 DisplayTitle(5, 10);
                 DrawText(TextFormat("Score: %i", Score), windowWidth * .33, windowHeight * .4, 55, ScoreColor);
