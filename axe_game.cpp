0cfa49f2f1cbcef264b8e7b42d194f716202ba42
dawkey95 <dawidkeyser95@gmail.com>
2022-07-15 10:21:38 +1200

    Completed Game

--- axe_game.cpp
+++ axe_game.cpp
@@ -1,125 +1,203 @@
 #include "raylib.h"
 #include "raymath.h"
 
+    //  Window Dimensions
+    int windowWidth = 800;
+    int windowHeight = 450;
+
+    //  Player Circle
+    int playerCenterX = 200;
+    int playerCenterY = 200;
+    int playerRadius = 25;
+
+    //  Obstacle Circle
+    int obstacleCenterX = 400;
+    int obstacleCenterY = 25;
+    int obstacleRadius = 25;
+    float directionV = 5;
+    float directionH = 5;
+
+    //  Collisions
+    float tolerance = playerRadius = obstacleRadius;
+    float goal = (5 * playerRadius) + obstacleRadius;
+    bool collisionWithObstacle{};
+    bool collisionWithScoringZone{};
+    float distance{};
+
+    //  Color Definitions
+    Color CYAN = {0, 255, 255, 255};
+    Color ScoreColor = CYAN;
+    Color PlayerColor = DARKGRAY;
+    Color ObstacleColor = MAROON;
+    Color BackgroundColor = BLACK;
+    Color Flicker[]{DARKGRAY, GRAY, LIGHTGRAY, WHITE};
+    int FlickerSize = 4;
+
+    //  Title
+    int BouncingAmount = 20;
+    int BouncingDirection = 1;
+    int BouncingOffset = 0;
+    int LetterOffset = 10;
+
+    // Gameplay
+    int PlayerSpeed = 10;
+    int Score = 0;
+    int Delay = 2*60;
+    int Timer = Delay;
+    int GameStarted = false;
+
+    void DisplayTitle(float PercentScreenWidth, float PercentScreenHeight)
+{   
+    if(BouncingOffset < -BouncingAmount || BouncingOffset > BouncingAmount) {
+        BouncingDirection = -BouncingDirection;        
+    }
+
+    BouncingOffset += BouncingDirection;
+    DrawText(TextFormat(" D   D   E   A   L "),
+                        (windowWidth*(PercentScreenWidth/100)) - LetterOffset, 
+                        (windowHeight*(PercentScreenHeight/100)) + BouncingOffset, 
+                        80, 
+                        ObstacleColor);
+
+    DrawText(TextFormat("   O   G   B   L   ! "), 
+                        (windowWidth*(PercentScreenWidth/100)), 
+                        (windowHeight*(PercentScreenHeight/100)) - BouncingOffset, 
+                        80, 
+                        ScoreColor);
+}
+
+bool UseWASDToProceed(float PercentScreenWidth, float PercentScreenHeight)
+{
+    // Current Game Time (reduced to a whole number) divided by the length of the Flicker array 
+    // leaves me a remainder I may use to count through the colors  Modulus FTW!
+    int Sequencer = (int)GetTime() % FlickerSize; 
+    DrawText("Press W, A, S, or D\n  or   Esc to Quit", (windowWidth * (PercentScreenWidth / 100)), (windowHeight * (PercentScreenHeight / 100)), 15, Flicker[Sequencer]);
+    if(IsKeyDown(KEY_W) || IsKeyDown(KEY_A) || IsKeyDown(KEY_S) || IsKeyDown(KEY_D))
+    {
+        return true;
+    }
+    return false;
+}
+
+void UpdateDistanceCheck()
+{
+    distance = sqrt(pow(playerCenterX - obstacleCenterX, 2) + pow(playerCenterY - obstacleCenterY, 2));
+    collisionWithObstacle = distance < tolerance;
+    collisionWithScoringZone = distance < goal;
+}
+
+void ResetGameVariables()
+{
+    playerCenterX = 200;
+    playerCenterY = 200;
+    obstacleCenterX = 400;
+    obstacleCenterY = 25; 
+    Timer = Delay;
+    Score = 0;
+    collisionWithObstacle = false;
+}
+
+void CalculateAndIndicateScoring()
+{
+    if (collisionWithScoringZone)
+    {
+        // increase score based on the distance from the obstacle but never less than 1 
+        Score += ((goal-distance)/playerRadius)+1;
+        // recolor the obstacle
+        DrawCircle(obstacleCenterX, obstacleCenterY, obstacleRadius, ScoreColor);   
+    }
+}
+
+void DrawScorePlayerAndObstacle()
+{
+    DrawText(TextFormat("Score: %i", Score), windowWidth*.85, windowHeight*.9, 20, ScoreColor);
+    DrawCircle(playerCenterX, playerCenterY, playerRadius, PlayerColor);
+    DrawCircle(obstacleCenterX, obstacleCenterY, obstacleRadius, ObstacleColor);
+}
+
+void BouncyBallMovement()
+{
+    obstacleCenterY += directionV;
+    if (obstacleCenterY > (windowHeight-obstacleRadius) || (obstacleCenterY < obstacleRadius))
+    {
+        directionV = -directionV;
+    }
+    obstacleCenterX += directionH;
+    if (obstacleCenterX > (windowWidth-obstacleRadius) || (obstacleCenterX < obstacleRadius))
+    {
+        directionH = -directionH;
+    }    
+}
+
+void ProcessGameControls()
+{
+    if (IsKeyDown(KEY_D) && (playerCenterX+playerRadius) < windowWidth)
+    {
+        playerCenterX += PlayerSpeed;
+    }
+    if (IsKeyDown(KEY_A) && (playerCenterX-playerRadius) > 0)
+    {
+        playerCenterX -= PlayerSpeed;
+    }
+    if (IsKeyDown(KEY_W) && (playerCenterY-playerRadius) > 0)
+    {
+        playerCenterY = playerCenterY - PlayerSpeed;
+    }
+    if (IsKeyDown(KEY_S) && (playerCenterY+playerRadius) < windowHeight)
+    {
+        playerCenterY = playerCenterY + PlayerSpeed;
+    }
+}
+
 int main()
 {
-
-    // Variables
-    int width;
-    int height;
-    int direction;
-    bool collision_with_axe;
-    bool gameStarted;
-
-    // Circle Coordinates
-    int circle_x;
-    int circle_y;
-    int circle_radius;
-
-    // Circle Edges
-    int l_circle_x{circle_x - circle_radius};
-    int r_circle_x{circle_x + circle_radius};
-    int u_circle_y{circle_y - circle_radius};
-    int b_circle_y{circle_y + circle_radius};
-
-    // Axe Coordinates
-    int axe_x;
-    int axe_y;
-    int axe_length;
-
-    // Axe Edges
-    int l_axe_x{axe_x};
-    int r_axe_x{axe_x + axe_length};
-    int u_axe_y{axe_y};
-    int b_axe_y{axe_y + axe_length};
-
-    // Variables Initialized
-    width = 800;
-    height = 450;
-    circle_x = 200;
-    circle_y = 200;
-    circle_radius = 25;
-    axe_x = 400;
-    axe_y = 0;
-    axe_length = 50;
-    direction = 10;
-    gameStarted = false;
-    collision_with_axe = 
-                    (b_axe_y >= u_circle_y) && 
-                    (u_axe_y <= b_circle_y) && 
-                    (l_axe_x <= r_circle_x) && 
-                    (r_axe_x >= l_circle_x);
-
-    // Main Code
-    InitWindow(width, height, "Axe Game");
-
+    InitWindow(windowWidth, windowHeight, "W/A/S/D Finger Trainer");
     SetTargetFPS(60);
-    while(WindowShouldClose() == false) {
-        // Update Cicrcle Edges
-        l_circle_x = circle_x - circle_radius;
-        r_circle_x = circle_x + circle_radius;
-        u_circle_y = circle_y - circle_radius;
-        b_circle_y = circle_y + circle_radius;
 
-        // Update Axe Edges
-        l_axe_x = axe_x;
-        r_axe_x = axe_x + axe_length;
-        u_axe_y = axe_y;
-        b_axe_y = axe_y + axe_length;
-
-        // Update Collision With Axe
-        collision_with_axe = 
-                    (b_axe_y >= u_circle_y) && 
-                    (u_axe_y <= b_circle_y) && 
-                    (l_axe_x <= r_circle_x) && 
-                    (r_axe_x >= l_circle_x);
-
-        BeginDrawing();
-        ClearBackground(WHITE);
-        
-        if (!gameStarted) {
-            DrawText("Press SPACE to play!", (width / 6), (height / 2), 50, BLACK);
-            if (IsKeyDown(KEY_SPACE)) {
-                gameStarted = true;
+    while(!WindowShouldClose())          
+    {
+        BeginDrawing();                  
+        ClearBackground(BackgroundColor);      
+        UpdateDistanceCheck();
+        // Start Screen
+        if (!GameStarted)
+        {
+            DisplayTitle(5, 40);
+            if(UseWASDToProceed(40, 70))
+            {
+                GameStarted = true;
             }
         }
-
-        else {
-            if (collision_with_axe){
-                DrawText("Game Over", (width / 2), (height / 2), 20, RED);
+        else
+        {
+            // End Screen   
+            if(collisionWithObstacle)
+            {
+                DisplayTitle(5, 10);
+                DrawText(TextFormat("Score: %i", Score), windowWidth * .33, windowHeight * .4, 55, ScoreColor);
+                DrawText("Did You Improve?", windowWidth * .33, windowHeight * .5, 30, ObstacleColor);
+                if(Timer <= 0)
+                {
+                    DrawText("Try Again!", windowWidth * .42, windowHeight * .58, 20, PlayerColor);
+                    if(UseWASDToProceed(40, 65))
+                    {
+                        ResetGameVariables();
+                    }
+                }
+                else
+                {
+                    Timer--;
+                }
             }
-
-            else {
-                // Game logic begins here
-                DrawCircle(circle_x, circle_y, circle_radius, BLUE);
-                DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
-
-                // Move The Axe
-                axe_y += direction;
-                if (axe_y > height || axe_y < 0) {
-                    direction = -direction;
-                }
-                    
-                // Move Circle
-                if (IsKeyDown(KEY_D) && circle_x < width) {
-                    circle_x += 10;
-                }
-
-                if (IsKeyDown(KEY_A) && circle_x > 0) {
-                    circle_x -= 10;
-                }
-
-                if (IsKeyDown(KEY_W) && circle_y > 0) {
-                    circle_y -= 10;
-                }
-
-                if (IsKeyDown(KEY_S) && circle_y < height) {
-                    circle_y += 10;
-                }
-                // Game logic ends here
-            }
+            // GamePlay
+            else
+            {
+                DrawScorePlayerAndObstacle();
+                CalculateAndIndicateScoring();
+                BouncyBallMovement();
+                ProcessGameControls();
+            } 
         }
-
-        EndDrawing();
+    EndDrawing();
     }
 }
